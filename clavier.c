ebadcc698e485c703a6b98073bfe3dc4cdfe04c4
MedMzi <167648834+MedMzi@users.noreply.github.com>
2024-05-15 18:47:59 +0100

    Update clavier.c

--- clavier.c
+++ clavier.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <time.h>
 
 #include "fonctions.h"
