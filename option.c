f0234359177ce0308e8e93e464ee7741e939ab70
MedMzi <167648834+MedMzi@users.noreply.github.com>
2024-05-15 13:19:29 +0100

    Add files via upload

--- option.c
+++ option.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 #include "fonctions.h"   
     
@@ -15,7 +15,7 @@ void initoption(){
 	afficherTexte(renderer, font, "Options", 300, 50);
 	char text[9]="3 Lettres";
 	SDL_SetRenderDrawColor(renderer, 150, 50, 100, 40); // Couleur gris clair
-	for (int i = 3; i < 8; i++) {    
+	for (int i = 3; i <= 8; i++) {    
 		text[0]='0'+i;	
 		SDL_Rect cadre = {250, 120+(i-3)*60, 300, 50};
 		
@@ -23,25 +23,30 @@ void initoption(){
 		
 		afficherTexte(renderer, font, text, cadre.x + cadre.w / 2 - 40, cadre.y + cadre.h / 2 - 12);
 	}
+	
+	newscene=false;
 }
 
 
-void option(SDL_Event event){
+bool option(SDL_Event event){
 
 	if (event.type == SDL_MOUSEBUTTONDOWN) {
 		if (event.button.button == SDL_BUTTON_LEFT) {
 			int x = event.button.x;
 			int y = event.button.y;
 			
-			for (int i = 3; i < 8; i++) {  
+			for (int i = 3; i <= 8; i++) {  
 				if (x >= 250 && x <= 550 && y >= (120+(i-3)*60) && y <= (170+(i-3)*60)) {
 					difficulte[0]='0'+i;
+
 					scene=0;
-					break;
+					newscene=true;
+					return false;
 				}	
  			}                       
 		}
-	}	
+	}
+	return true;	
 }
 
 
